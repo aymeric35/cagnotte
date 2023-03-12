4e6a5332728874d03568b0c1f00b42ca325c0edc
aymeric35 <Oshi2606@gmail.com>
2023-03-12 15:30:06 +0100

    🔧 Configure layers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    extends: [
+        "../../layers/firebase",
+        "../../layers/daisyui",
+    ],
 })
