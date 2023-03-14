62f801c56df3d4c5b75d3a5f864ea994baa2bda6
aymeric35 <Oshi2606@gmail.com>
2023-03-14 21:05:50 +0100

    ✨ Add firebase login logic

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <NuxtPage />
   </div>
 </template>
+
+<script lang="ts" setup>
+
+const router = useRouter()
+const route = useRoute()
+const user = useCurrentUser()
+
+onMounted(() => {
+  watch(user, (user, prevUser) => {
+      if (prevUser && !user) {
+          // user logged out
+          router.push('/login')
+      } else if (user && typeof route.query.redirect === 'string') {
+          // user logged in
+          router.push(route.query.redirect)
+      }
+  })
+})
+</script>
