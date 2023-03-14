6212bf2711ac0ed7fa45b78bf00cca8f7c843720
aymeric35 <Oshi2606@gmail.com>
2023-03-14 21:06:36 +0100

    ✨ Add register page

---
+++ pages/register.vue
@@ -0,0 +1,9 @@
+<template>
+    <DaisyAuthRegister :withCredentials="registerUser" :withGoogle="signInWithGoogle" @register="navigateTo('/')">
+    </DaisyAuthRegister>
+</template>
+  
+<script lang="ts" setup>
+
+
+</script>
