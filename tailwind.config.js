c10ab996a3ce8d3e650f843717f29f85a6124589
aymeric35 <Oshi2606@gmail.com>
2023-03-14 21:04:23 +0100

    🔧 Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    // see https://daisyui.com/docs/config/ for configuration
+    plugins: [require("daisyui")],
+    daisyui: {
+      themes: ["system", "dark", "cmyk"],
+    },
+    safelist: [
+      "alert",
+      "alert-success",
+      "alert-error",
+      "alert-warning",
+      "alert-info",
+    ],
+  };
