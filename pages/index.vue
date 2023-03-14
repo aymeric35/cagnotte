24e951a79fc0007522db98cef27eb9a8c94e09d2
aymeric35 <Oshi2606@gmail.com>
2023-03-14 21:06:12 +0100

    ✨ Add basic homepage

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Hello {{ user?.displayName }}
+        <button class="btn" @click="signOutUser"> Se deconnecter</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const user = useCurrentUser()
+
+</script>
+
+<style scoped></style>
