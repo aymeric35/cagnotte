c5bad9b8e2675e2168c153a7712a3d933f06610c
aymeric35 <Oshi2606@gmail.com>
2023-03-14 21:06:24 +0100

    ✨ Add login page

---
+++ pages/login.vue
@@ -0,0 +1,8 @@
+<template>
+    <DaisyAuthLogin :withCredentials="signInUser" :withGoogle="signInWithGoogle"
+        :forgotPassword="sendForgotPasswordResetEmail" @login="navigateTo('/')"></DaisyAuthLogin>
+</template>
+
+<script lang="ts" setup>
+
+</script>
