a75ad9d1c13fb2e886879d2f31cacd9849ae4e50
Aymeric <aymeric.bertrand35@gmail.com>
2023-03-24 19:22:54 +0100

    🗑️ Clean unused script tags

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,3 @@
     <DaisyAuthLogin :withCredentials="signInUser" :withGoogle="signInWithGoogle"
         :forgotPassword="sendForgotPasswordResetEmail" @login="navigateTo('/')"></DaisyAuthLogin>
 </template>
-
-<script lang="ts" setup>
-
-</script>
