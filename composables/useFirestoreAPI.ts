41b7072f4079fcb60985da4f00607722b8f69ff6
aymeric35 <Oshi2606@gmail.com>
2023-04-20 19:03:42 +0200

    ♻️ Refactor composable

--- composables/useFirestoreAPI.ts
+++ composables/useFirestoreAPI.ts
@@ -2,8 +2,9 @@ import { collection, doc } from 'firebase/firestore'
 export default function useFirestoreAPI() {
     const db = useFirestore();
 
-    const data = useState('data', () => useDocument(doc(collection(db, 'app'), 'data')).data)
-    const pending = useState('pending', () => useDocument(doc(collection(db, 'app'), 'data')).pending)
+    const { data, pending } = useDocument(doc(collection(db, 'app'), 'data'))
 
     return { data, pending }
 }
+    // const api = useState('data', () => ({ pending: true, data: null }))
+    // return useDocument(doc(collection(db, 'app'), 'data'), { target: api })
