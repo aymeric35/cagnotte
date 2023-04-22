cc0ab2287bc4d23bdaaaaa88eff2cbebdeb07791
Aymeric <aymeric.bertrand35@gmail.com>
2023-04-22 17:43:20 +0200

    ♻️ Refactor stats page to use firebase

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
-const { data, pending } = useFirestoreAPI();
-
-
+const { data, pending } = useFirestoreAPI('users');
+const usersList = computed(() => {
+    if (data.value) {
+        return data.value
+    }
+})
 </script>
 
 <template>
     <section>
-        <div class="stats stats-horizontal shadow">
-
-            <div class="stat">
-                <div class="stat-title">Downloads</div>
-                <div class="stat-value">31K</div>
-                <div class="stat-desc">Jan 1st - Feb 1st</div>
-            </div>
-
-            <div class="stat">
-                <div class="stat-title">New Users</div>
-                <div class="stat-value">4,200</div>
-                <div class="stat-desc">↗︎ 400 (22%)</div>
-            </div>
-
-            <div class="stat">
-                <div class="stat-title">New Registers</div>
-                <div class="stat-value">1,200</div>
-                <div class="stat-desc">↘︎ 90 (14%)</div>
-            </div>
-
+        <div v-if="usersList" class="stats stats-horizontal shadow">
+            <template v-for="(user, name) in usersList" :key="user.id">
+                <div class="stat">
+                    <div class="stat-title">{{ name }}</div>
+                    <div class="stat-value"> {{ Number(user.euros).toFixed(2) }}</div>
+                    <div class="stat-desc"> {{ user.id }} </div>
+                </div>
+            </template>
         </div>
     </section>
 </template>
