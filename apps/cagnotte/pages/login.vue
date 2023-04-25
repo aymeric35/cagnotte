591f639ba9b988b7da50838d348a8cfc529028f3
Aymeric <aymeric.bertrand35@gmail.com>
2023-04-25 18:51:21 +0200

    ✨ Add specific header for login page

--- apps/cagnotte/pages/login.vue
+++ apps/cagnotte/pages/login.vue
@@ -1,4 +1,13 @@
 <template>
+  <header>
+    <div class="navbar bg-base-200">
+      <div class="flex-1">
+        <NuxtLink class="btn btn-ghost normal-case text-xl" to="/">
+          Cagnotte Vacances
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
   <DaisyAuthLogin
     :with-credentials="signInUser" :with-google="signInWithGoogle"
     :forgot-password="sendForgotPasswordResetEmail" @login="navigateTo('/')"
